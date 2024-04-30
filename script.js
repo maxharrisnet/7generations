be68b1735878d6602d8a100fb624e81e519d2920
maxharrisnet <hello@maxharris.codes>
2024-04-30 16:05:51 -0700

    js for banner

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// JavaScript to scroll in the list items like a marquee
+// JavaScript to create a continuous scrolling marquee effect
 const banners = document.querySelectorAll(".hero-banner");
 
 banners.forEach(banner => {
 	const listItems = banner.querySelectorAll("li");
-	let index = 0;
+	let delay = 0;
 
-	setInterval(() => {
-		if (index > 0) {
-			listItems[index - 1].style.opacity = 0;
-			listItems[index - 1].style.transform = "translateX(-100%)";
-		}
-
-		listItems[index].style.opacity = 1;
-		listItems[index].style.transform = "translateX(0)";
-
-		index = (index + 1) % listItems.length;
-	}, 2000);
+	listItems.forEach((item, index) => {
+		item.style.animation = `marquee 10s linear ${delay}s infinite`;
+		item.style.opacity = 1;
+		delay += 2;
+	});
 });
